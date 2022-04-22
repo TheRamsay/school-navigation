a5105da6a19327c1afb6eb6d3a6b5d80525f53c1
TheRamsay <ramsayofbolton@gmail.com>
2022-04-22 22:12:41 +0200

    registered new models in admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,13 @@
-from api.models import Employee, Room
+from api.models import Employee, Room, Title, EmployeeRoom, EmployeeTitle
 from django.contrib import admin
 
-# Register your models here.
 admin.site.register(Room)
 admin.site.register(Employee)
+admin.site.register(Title)
+admin.site.register(EmployeeTitle)
+admin.site.register(EmployeeRoom)
+
+
+
+
+
