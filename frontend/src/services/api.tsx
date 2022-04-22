f690d5453a132eabd52d05e662090c7d773bb3d9
TheRamsay <ramsayofbolton@gmail.com>
2022-04-22 22:20:03 +0200

    new endpoint for user by room

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -3,28 +3,14 @@ import axios from "axios";
 // const BASE_URL = "http://78.47.245.96/api/";
 const BASE_URL = "http://localhost:8000/api/";
 
-// const makeQuery = (query) => {
-//     const data = new FormData();
-//     data.append("query", query);
-//     return axios.post(BASE_URL + "search/", data).then((res) => res.data);
-// };
-
-export const axiosInstance = axios.create({
-    // withCredentials: true,
-    // headers: {
-    //     "X-CSRFToken": Cookies.get("csrftoken"),
-    // },
-});
+export const axiosInstance = axios.create({});
 
 const makeQuery = async (query: string): Promise<any> => {
     const res = await axiosInstance.get(`${BASE_URL}search/?query=${query}`);
     return res.data;
-    // return axios
-    //     .get(BASE_URL + "search/?query=" + query)
-    //     .then((res) => res.data);
 };
 
-const getRoom = (id: string) => {
+const getRoom = (id: string): Promise<any> => {
     return axiosInstance.get(`${BASE_URL}room/${id}`).then((res) => res.data);
 };
 
@@ -32,4 +18,8 @@ const getEmployee = (id: string) => {
     return axiosInstance.get(`${BASE_URL}employee/${id}`).then((res) => res.data);
 };
 
-export { makeQuery, getRoom, getEmployee };
+
+const getEmployeeInRoom = (employeeID: string, roomID: string) => {
+    return axiosInstance.get(`${BASE_URL}employee/${employeeID}/room/${roomID}`).then((res) => res.data);
+};
+export {makeQuery, getRoom, getEmployee, getEmployeeInRoom};
