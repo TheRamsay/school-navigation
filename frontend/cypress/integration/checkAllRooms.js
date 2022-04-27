4af96117e6d0b1aace7f846ba34cbdb26931ad4e
TheRamsay <ramsayofbolton@gmail.com>
2022-04-27 22:20:31 +0200

    added rooms that are not on the map to ignore list

--- frontend/cypress/integration/checkAllRooms.js
+++ frontend/cypress/integration/checkAllRooms.js
@@ -4,7 +4,7 @@ describe("Checking if all rooms are clickable", function () {
 		cy.request("http://127.0.0.1:8000/api/room/floor/1").then((data) => {
 			const x = data.body.map(k => k.room_id);
 			cy.wrap(x).each(j => {
-				if ([212, 214, 214].includes(+j)) {
+				if ([212, 213, 214, 214, 2222].includes(+j)) {
 					return;
 				}
 
